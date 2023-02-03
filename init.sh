b90a85bff4ad9b2a623d293a9faa1b0c30a2a1c4
saviocosmo <123900209+saviocosmo@users.noreply.github.com>
2023-02-02 23:46:42 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #chmod +x 
 
 
-git clone https://github.com/lucasvieceli/bombcrypto-superbot.git
+git clone https://github.com/saviocosmo/bombcrypto-superbot.git
 cd bombcrypto-superbot
 yarn install
 yarn start:init
